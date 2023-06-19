72cd03dfe477767798de4da9af2ae65d486427bc
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2023-06-20 03:20:26 +0600

    MR X Doing  Beginner Project

---
+++ beginner-website-project/app/Http/Controllers/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ContactController extends Controller
+{
+    function page(Request $request){
+        return view('pages.contact');
+    }
+
+    function contactRequest(Request $request){
+        return DB::table('contacts')->insert($request->input());
+    }
+}
