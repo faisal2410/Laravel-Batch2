c2cb4e0a2f501d1b0851198009f739c4c938f111
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2023-06-22 02:03:02 +0600

    HTML to Laravel Blade Conversion

--- beginner-website-project/resources/views/app.blade.php
+++ beginner-website-project/resources/views/app.blade.php
@@ -6,18 +6,25 @@
     <meta name="description" content=""/>
     <meta name="author" content=""/>
     <title>MR-X</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('/favicon.ico')}}" />
+    <link href="{{asset('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/style.css')}}" rel="stylesheet" />
 </head>
 
-<body>
+<body class="d-flex flex-column h-100">
 
 @include('components.navbar')
-
 @include('components.loader')
-
 <div class="" id="content-div">
     @yield('content')
 </div>
 
 @include('components.footer')
+
+
+
+<script src="{{asset('js/axios.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
 </body>
 </html>
--- beginner-website-project/resources/views/pages/projects.blade.php
+++ beginner-website-project/resources/views/pages/projects.blade.php
@@ -1,4 +1,5 @@
 @extends('app')
+
 @section('content')
     @include('components.project-list')
     @include('components.call-action')
