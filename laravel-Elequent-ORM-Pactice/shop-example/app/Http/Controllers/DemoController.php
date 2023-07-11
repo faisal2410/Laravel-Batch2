7d6c808b6bf7ea28f25e3ed9525f72fa0a870458
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2023-07-11 21:38:37 +0600

    user managment start

--- laravel-Elequent-ORM-Pactice/shop-example/app/Http/Controllers/DemoController.php
+++ laravel-Elequent-ORM-Pactice/shop-example/app/Http/Controllers/DemoController.php
@@ -1,11 +1,18 @@
 <?php
 namespace App\Http\Controllers;
 use App\Models\Brand;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
 
 class DemoController extends Controller
 {
     public function DemoAction()
     {
-       return Brand::all();
+      return Product::paginate(
+          $perPage = 5,
+          $columns = ['*'],
+          $pageName = 'pageName'
+      );
     }
 }
--- laravel-Elequent-ORM-Pactice/shop-example/routes/api.php
+++ laravel-Elequent-ORM-Pactice/shop-example/routes/api.php
@@ -1,7 +1,11 @@
 <?php
+
+use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
 
 
 
 
+
+Route::post('/create-brand', [DemoController::class, 'DemoAction']);
