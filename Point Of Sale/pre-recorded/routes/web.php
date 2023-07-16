350d6dcc36e776e71c552f6266d8cbc9f0449fd9
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2023-07-16 18:08:32 +0600

    df
    sdfsf

--- Point Of Sale/pre-recorded/routes/web.php
+++ Point Of Sale/pre-recorded/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\TokenVerificationMiddleware;
 use Illuminate\Support\Facades\Route;
@@ -15,15 +16,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// API Routes
 Route::post('/user-registration',[UserController::class,'UserRegistration']);
 Route::post('/user-login',[UserController::class,'UserLogin']);
 Route::post('/send-otp',[UserController::class,'SendOTPCode']);
 Route::post('/verify-otp',[UserController::class,'VerifyOTP']);
-
-// Token Verify
 Route::post('/reset-password',[UserController::class,'ResetPassword'])->middleware([TokenVerificationMiddleware::class]);
 
 
+// Page Routes
+Route::get('/userLogin',[UserController::class,'LoginPage']);
+Route::get('/userRegistration',[UserController::class,'RegistrationPage']);
+Route::get('/sendOtp',[UserController::class,'SendOtpPage']);
+Route::get('/verifyOtp',[UserController::class,'VerifyOTPPage']);
+Route::get('/resetPassword',[UserController::class,'ResetPasswordPage']);
+
+Route::get('/dashboard',[DashboardController::class,'DashboardPage']);
 
 
 
